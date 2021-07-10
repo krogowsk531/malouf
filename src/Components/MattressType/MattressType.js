b6ef0c402647ec9ce125cac9f82c3bee7521f3e3
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 11:56:24 -0700

    Imported and added to css to the four types of mattress images

--- src/Components/MattressType/MattressType.js
+++ src/Components/MattressType/MattressType.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import rectangle from './Rectangle 3 Copy 3.png'
+import memoryFoam from './Memory Foam Feature Copy.png'
+import latexFoam from './Latex foam feature Copy.png'
+import spring from './Spring Feature Copy.png'
+import hybrid from './Hybrid Feature Copy.png'
 
 class MattressType extends Component {
   render() {
     return (
       <div>
         <img src={rectangle} className='blue-rectangle' alt='blue rectangle box' />
+        <img src={memoryFoam} className='memory-foam' alt='memory foam mattress info' />
+        <img src={latexFoam} className='latex-foam' alt='latex foam mattress info' />
+        <img src={spring} className='spring' alt='spring mattress info' />
+        <img src={hybrid} className='hybrid' alt='hybrid mattress info' />
       </div>
     )
   }
