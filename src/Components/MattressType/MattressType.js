7a0ba4729d34b47d4ad7f9aba262052c3be4d501
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 14:32:08 -0700

    All images imported and have css styling for MattressType component

--- src/Components/MattressType/MattressType.js
+++ src/Components/MattressType/MattressType.js
@@ -5,20 +5,25 @@ import latexFoam from './Latex foam feature Copy.png'
 import spring from './Spring Feature Copy.png'
 import hybrid from './Hybrid Feature Copy.png'
 import text from './Whats Your Type.png'
+import whiteLine from './Line 7 Copy.png'
 
 class MattressType extends Component {
   render() {
     return (
       <div>
+        <img src={text} className='text' alt='text whats your type' />
+        <img src={whiteLine} className='white-line' alt='white line for style' />
         <img src={rectangle} className='blue-rectangle' alt='blue rectangle box' />
         <img src={memoryFoam} className='memory-foam' alt='memory foam mattress info' />
         <img src={latexFoam} className='latex-foam' alt='latex foam mattress info' />
         <img src={spring} className='spring' alt='spring mattress info' />
         <img src={hybrid} className='hybrid' alt='hybrid mattress info' />
-        <img src={text} className='text' alt='text whats your type' />
       </div>
     )
   }
 }
 
 export default MattressType; 
+
+
+
