535651ff4d923afed4a25baae8de3a8521d874d9
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 07:02:50 -0700

    Import welcome text and render/fix

--- src/Components/WelcomeArea/WelcomeArea.js
+++ src/Components/WelcomeArea/WelcomeArea.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import mattress from './Mattresses-HeaderImage.svg';
 import square from './Rectangle 12 Copy.png';
 import shadow from './Rectangle 3.png'
+import welcome from './Good Things Come in.png'
 import './WelcomeArea.css';
 
 
@@ -10,7 +11,8 @@ class WelcomeArea extends Component {
     return (
       <div>
         <img src={square} className='blue-box' alt='blue square box' />
-        <p className='welcome-text'>Good Things Come in Tall But Manageable Packages</p>
+        <img src={welcome} className='welcome-text' alt='welcome message' />
+        <p className='small-welcome-text'>Linenspa mattresses feature a wide variety of feels and fibers. Firm to plush, memory foam to encased coils—it’s all here. Oh, and did we mention not a single bed is priced over $600? That’s right.</p>
         <img src={shadow} className='mattress-shadow' alt='blue shadow box' />
         <img src={mattress} className='mattress-image' alt='mattress' />
       </div>
