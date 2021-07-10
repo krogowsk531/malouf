de1cef02ae1702305a84e3f0e4ec40922d4aa333
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 10:56:13 -0700

    Imported grey square image and created css file for component

---
+++ src/Components/MattressSize/MattressSize.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import greySquare from './Rectangle 3 Copy 5.png'
+
+class MattressSize extends {
+  render() {
+    return (
+      <div>
+        <img src={greySquare} className='grey-square' alt='grey square box' />
+      </div>
+    )
+  }
+}
+
+export default MattressSize; 
