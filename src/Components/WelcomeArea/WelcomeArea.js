1573d03a6a199d5eb1b96b51c32d93ccadb631f9
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 00:30:24 -0700

    Added styling for little blue box next to welcome text

--- src/Components/WelcomeArea/WelcomeArea.js
+++ src/Components/WelcomeArea/WelcomeArea.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import mattress from './Mattresses-HeaderImage.svg';
+import square from './Rectangle 12 Copy.png';
 import './WelcomeArea.css';
 
 
@@ -7,6 +8,7 @@ class WelcomeArea extends Component {
   render() {
     return (
       <div>
+        <img src={square} className='blue-box' alt='blue-square-box' />
         <p className='welcome-text'>Good Things Come in Tall But Manageable Packages</p>
         <div className='mattress-shadow'></div>
         <img src={mattress} className='mattress-image' alt='mattress' />
