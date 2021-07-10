eef3ddd7298d264c1aa2e519f08218f8b2a27433
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 07:08:47 -0700

    Import small text and render/fix

--- src/Components/WelcomeArea/WelcomeArea.js
+++ src/Components/WelcomeArea/WelcomeArea.js
@@ -3,6 +3,7 @@ import mattress from './Mattresses-HeaderImage.svg';
 import square from './Rectangle 12 Copy.png';
 import shadow from './Rectangle 3.png'
 import welcome from './Good Things Come in.png'
+import linenspa from './Linenspa mattresses.png'
 import './WelcomeArea.css';
 
 
@@ -12,7 +13,7 @@ class WelcomeArea extends Component {
       <div>
         <img src={square} className='blue-box' alt='blue square box' />
         <img src={welcome} className='welcome-text' alt='welcome message' />
-        <p className='small-welcome-text'>Linenspa mattresses feature a wide variety of feels and fibers. Firm to plush, memory foam to encased coils—it’s all here. Oh, and did we mention not a single bed is priced over $600? That’s right.</p>
+        <img src={linenspa} className='small-welcome-text' alt='Info about Linenspa mattresses'/>
         <img src={shadow} className='mattress-shadow' alt='blue shadow box' />
         <img src={mattress} className='mattress-image' alt='mattress' />
       </div>
