9be504f915bcad7901568a52e49fdcf55d81e9b8
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-11 16:51:49 -0700

    Import and render mattress icon labels in MattressSize component

--- src/Components/MattressSize/MattressSize.js
+++ src/Components/MattressSize/MattressSize.js
@@ -11,6 +11,13 @@ import king from './Rectangle 4 Copy 23.png'
 import calKing from './Rectangle 4 Copy 24.png'
 import twinDescription from './Actual_ 37.5 x 74 in.png'
 import './MattressSize.css';
+import twinText from './Twin Copy.png'
+import twinXlText from './Twin XL Copy.png'
+import fullText from './FULL Copy.png'
+import fullXlText from './FULL XL Copy.png'
+import queenText from './Queen Copy.png'
+import kingText from './King Copy.png'
+import calKingText from './Cal King Copy.png'
 
 
 
@@ -22,12 +29,19 @@ class MattressSize extends Component {
         <img src={blueLine} className='blue-line' alt='blue line for styling' />
         <img src={guideText} className='guide-text' alt='text for A Guide for Mattress Sizes' />
         <img src={twin} className='twin' alt='icon for twin size bed' />
+        <img src={twinText} className='twin-text' alt='text to indicate shape is a twin bed' />
         <img src={twinXl} className='twin-xl' alt='icon for twin xl size bed' />
+        <img src={twinXlText} className='twin-xl-text' alt='text to indicate shape is a twin xl bed' />
         <img src={full} className='full' alt='icon for full size bed' />
+        <img src={fullText} className='full-text' alt='text to indicate shape is a full bed' />
         <img src={fullXl} className='full-xl' alt='icon for full xl size bed' />
+        <img src={fullXlText} className='full-xl-text' alt='text to indicate shape is a full xl bed' />
         <img src={queen} className='queen' alt='icon for queen size bed' />
+        <img src={queenText} className='queen-text' alt='text to indicate shape is a queen bed' />
         <img src={king} className='king' alt='icon for king size bed' />
+        <img src={kingText} className='king-text' alt='text to indicate shape is a king bed' />
         <img src={calKing} className='cal-king' alt='icon for california king size bed' />
+        <img src={calKingText} className='cal-king-text' alt='text to indicate shape is a california king bed' />
         <img src={twinDescription} className='twin-description' alt='text to describe the twin mattress' />
       </div>
     )
