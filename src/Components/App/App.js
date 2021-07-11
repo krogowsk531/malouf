91ebacb3d55d29768339cbe63b231d524c3f78fd
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-10 21:33:53 -0700

    Wrote basic implementation code so that the Tips component will render

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         <MattressSize />
         <MattressType />
         <ProductCards />
+        <Tips />
       </div>
     )
   }
---
+++ src/Components/Tips/Tips.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Tips extends Components {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default Tips;
