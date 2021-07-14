0fe2a596c10ee4336a5aa92ab82413cb804be22d
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-14 01:59:57 -0700

    Built skelton from ProductCardsHorizontalTab component

---
+++ src/Components/ProductCardsHorizontalTab/ProductCardsHorizontalTab.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ProductCardsHorizontalTab extends Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default ProductCardsHorizontalTab;
