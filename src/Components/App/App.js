4cded6656ab87e1cddea5cb85713a82a9a7a84bc
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-17 09:17:11 -0700

    Added conditiional rendering for desktop and horizontal welcomeArea components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,16 +10,47 @@ import WelcomeAreaHorizontalTab from '../WelcomeAreaHorizontalTab/WelcomeAreaHor
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDesktop: false
+    }
+    
+    this.updatePredicate = this.updatePredicate.bind(this)
+  }
+  
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener('resize', this.updatePredicate)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updatePredicate)
+  }
+  
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 1024 })
+  }
+  
   render() {
+    const isDesktop = this.state.isDesktop;
+    
     return (
       <div>
-        {/* <WelcomeArea />*/}
-        <WelcomeAreaHorizontalTab />
+      <div>
+        {isDesktop ? (
+          <WelcomeArea />
+        ) : (
+          <WelcomeAreaHorizontalTab />
+        )}
+      </div>
+      <div>
         <MattressSize />
         <MattressType />
         {/*<ProductCards /> */}
-        <ProductCardsHorizontalTab />
+        {/*<ProductCardsHorizontalTab /> */}
         <Tips />
+      </div>
       </div>
     )
   }
