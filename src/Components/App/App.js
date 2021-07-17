34ce44ac34d37a42978c0071cc52479f2aefa4a2
Katy St Sauveur <katyrogowski@yahoo.com>
2021-07-17 10:57:45 -0700

    Initial styling on WelcomeArea for portrait tablet complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,43 +7,45 @@ import ProductCards from '../ProductCards/ProductCards';
 import Tips from '../Tips/Tips';
 import ProductCardsHorizontalTab from '../ProductCardsHorizontalTab/ProductCardsHorizontalTab'
 import WelcomeAreaHorizontalTab from '../WelcomeAreaHorizontalTab/WelcomeAreaHorizontalTab'
+import WelcomeAreaPortraitTab from '../WelcomeAreaPortraitTab/WelcomeAreaPortraitTab'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDesktop: false
-    }
-    
-    this.updatePredicate = this.updatePredicate.bind(this)
-  }
-  
-  componentDidMount() {
-    this.updatePredicate();
-    window.addEventListener('resize', this.updatePredicate)
-  }
-  
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updatePredicate)
-  }
-  
-  updatePredicate() {
-    this.setState({ isDesktop: window.innerWidth > 1024 })
-  }
-  
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isDesktop: false
+  //   }
+  // 
+  //   this.updatePredicate = this.updatePredicate.bind(this)
+  // }
+  // 
+  // componentDidMount() {
+  //   this.updatePredicate();
+  //   window.addEventListener('resize', this.updatePredicate)
+  // }
+  // 
+  // componentWillUnmount() {
+  //   window.removeEventListener('resize', this.updatePredicate)
+  // }
+  // 
+  // updatePredicate() {
+  //   this.setState({ isDesktop: window.innerWidth > 1024 })
+  // }
+  // 
   render() {
-    const isDesktop = this.state.isDesktop;
+    // const isDesktop = this.state.isDesktop;
     
     return (
       <div>
-      <div>
+      {/*<div>
         {isDesktop ? (
           <WelcomeArea />
         ) : (
           <WelcomeAreaHorizontalTab />
         )}
-      </div>
+      </div>*/}
+        <WelcomeAreaPortraitTab />
       <div>
         <MattressSize />
         <MattressType />
